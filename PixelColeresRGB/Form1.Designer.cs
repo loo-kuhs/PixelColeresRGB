fee1b9d23ce2661dd93755461bce97eac7a3a3fa
lucasB_py <upt.carlos.b.lucas.a@gmail.com>
2021-03-16 19:58:54 -0600

    Arreglado el bug de los labels

--- PixelColeresRGB/Form1.Designer.cs
+++ PixelColeresRGB/Form1.Designer.cs
@@ -31,11 +31,11 @@ namespace PixelColeresRGB
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtRojo = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtVerde = new System.Windows.Forms.Label();
-            this.txtAzul = new System.Windows.Forms.Label();
+            this.txtVerde = new System.Windows.Forms.TextBox();
+            this.txtAzul = new System.Windows.Forms.TextBox();
+            this.lblRojo = new System.Windows.Forms.Label();
+            this.lblVerde = new System.Windows.Forms.Label();
+            this.lblAzul = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -60,58 +60,58 @@ namespace PixelColeresRGB
             this.txtRojo.Size = new System.Drawing.Size(100, 20);
             this.txtRojo.TabIndex = 1;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(70, 70);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(70, 108);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 32);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Rojo";
-            // 
             // txtVerde
             // 
-            this.txtVerde.AutoSize = true;
-            this.txtVerde.Location = new System.Drawing.Point(29, 73);
+            this.txtVerde.Location = new System.Drawing.Point(70, 70);
             this.txtVerde.Name = "txtVerde";
-            this.txtVerde.Size = new System.Drawing.Size(35, 13);
-            this.txtVerde.TabIndex = 5;
-            this.txtVerde.Text = "Verde";
+            this.txtVerde.Size = new System.Drawing.Size(100, 20);
+            this.txtVerde.TabIndex = 2;
             // 
             // txtAzul
             // 
-            this.txtAzul.AutoSize = true;
-            this.txtAzul.Location = new System.Drawing.Point(29, 115);
+            this.txtAzul.Location = new System.Drawing.Point(70, 108);
             this.txtAzul.Name = "txtAzul";
-            this.txtAzul.Size = new System.Drawing.Size(27, 13);
-            this.txtAzul.TabIndex = 6;
-            this.txtAzul.Text = "Azul";
+            this.txtAzul.Size = new System.Drawing.Size(100, 20);
+            this.txtAzul.TabIndex = 3;
+            // 
+            // lblRojo
+            // 
+            this.lblRojo.AutoSize = true;
+            this.lblRojo.Location = new System.Drawing.Point(29, 32);
+            this.lblRojo.Name = "lblRojo";
+            this.lblRojo.Size = new System.Drawing.Size(29, 13);
+            this.lblRojo.TabIndex = 4;
+            this.lblRojo.Text = "Rojo";
+            // 
+            // lblVerde
+            // 
+            this.lblVerde.AutoSize = true;
+            this.lblVerde.Location = new System.Drawing.Point(29, 73);
+            this.lblVerde.Name = "lblVerde";
+            this.lblVerde.Size = new System.Drawing.Size(35, 13);
+            this.lblVerde.TabIndex = 5;
+            this.lblVerde.Text = "Verde";
+            // 
+            // lblAzul
+            // 
+            this.lblAzul.AutoSize = true;
+            this.lblAzul.Location = new System.Drawing.Point(29, 115);
+            this.lblAzul.Name = "lblAzul";
+            this.lblAzul.Size = new System.Drawing.Size(27, 13);
+            this.lblAzul.TabIndex = 6;
+            this.lblAzul.Text = "Azul";
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.txtAzul);
+            this.groupBox1.Controls.Add(this.lblAzul);
             this.groupBox1.Controls.Add(this.txtRojo);
+            this.groupBox1.Controls.Add(this.lblVerde);
             this.groupBox1.Controls.Add(this.txtVerde);
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox3);
-            this.groupBox1.Location = new System.Drawing.Point(453, 122);
+            this.groupBox1.Controls.Add(this.lblRojo);
+            this.groupBox1.Controls.Add(this.txtAzul);
+            this.groupBox1.Location = new System.Drawing.Point(451, 98);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 147);
+            this.groupBox1.Size = new System.Drawing.Size(216, 155);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "RGB";
@@ -137,11 +137,11 @@ namespace PixelColeresRGB
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtRojo;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label txtVerde;
-        private System.Windows.Forms.Label txtAzul;
+        private System.Windows.Forms.TextBox txtVerde;
+        private System.Windows.Forms.TextBox txtAzul;
+        private System.Windows.Forms.Label lblRojo;
+        private System.Windows.Forms.Label lblVerde;
+        private System.Windows.Forms.Label lblAzul;
         private System.Windows.Forms.GroupBox groupBox1;
     }
 }
