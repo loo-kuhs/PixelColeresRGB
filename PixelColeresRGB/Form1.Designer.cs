ece3373a6011659d58624e0424db322c7a42341b
lucasB_py <upt.carlos.b.lucas.a@gmail.com>
2021-03-16 19:54:44 -0600

    Agregar archivos de proyecto.

---
+++ PixelColeresRGB/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace PixelColeresRGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtRojo = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVerde = new System.Windows.Forms.Label();
+            this.txtAzul = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = global::PixelColeresRGB.Properties.Resources.HLSColorSpace;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(404, 404);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // txtRojo
+            // 
+            this.txtRojo.Location = new System.Drawing.Point(70, 29);
+            this.txtRojo.Name = "txtRojo";
+            this.txtRojo.Size = new System.Drawing.Size(100, 20);
+            this.txtRojo.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rojo";
+            // 
+            // txtVerde
+            // 
+            this.txtVerde.AutoSize = true;
+            this.txtVerde.Location = new System.Drawing.Point(29, 73);
+            this.txtVerde.Name = "txtVerde";
+            this.txtVerde.Size = new System.Drawing.Size(35, 13);
+            this.txtVerde.TabIndex = 5;
+            this.txtVerde.Text = "Verde";
+            // 
+            // txtAzul
+            // 
+            this.txtAzul.AutoSize = true;
+            this.txtAzul.Location = new System.Drawing.Point(29, 115);
+            this.txtAzul.Name = "txtAzul";
+            this.txtAzul.Size = new System.Drawing.Size(27, 13);
+            this.txtAzul.TabIndex = 6;
+            this.txtAzul.Text = "Azul";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAzul);
+            this.groupBox1.Controls.Add(this.txtRojo);
+            this.groupBox1.Controls.Add(this.txtVerde);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(453, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 147);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RGB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 433);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtRojo;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtVerde;
+        private System.Windows.Forms.Label txtAzul;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ PixelColeresRGB/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using IAupt.uptRNA;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelColeresRGB
+{
+    public partial class Form1 : Form
+    {
+        List<String> colores = new List<string> { "Fuera de la imagen",
+        "Verde", "Azul claro", "Azul fuerte", "Rosa", "Rojo", "Amarillo",
+        "Verde/Amarillo", "Verde/Azul claro" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap imagen = new Bitmap(pictureBox1.Image);
+
+            txtRojo.Text = imagen.GetPixel(e.X, e.Y).R.ToString();
+            txtVerde.Text = imagen.GetPixel(e.X, e.Y).G.ToString();
+            txtAzul.Text = imagen.GetPixel(e.X, e.Y).B.ToString();
+
+            PerceptronMultiCapa rna = new PerceptronMultiCapa(@"ejemplo.ppm");
+
+            double[] x = { imagen.GetPixel(e.X, e.Y).R, imagen.GetPixel(e.X, e.Y).G, imagen.GetPixel(e.X, e.Y).B };
+            rna.reconocer(x);
+
+            double[,] y = rna.y;
+
+            MessageBox.Show("El color es " + colores[(int)Math.Round(y[0, 0], 0)]);
+        }
+    }
+}
